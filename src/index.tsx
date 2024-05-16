26d1768f262dde1d9c19986c40445efdc33083e7
Pankaj Chaudhary <pankajchaudhary281@gmail.com>
2024-05-16 17:26:36 +1000

    Fixing readme and renaming

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles.css';
-import App from './App';
+import App from './ReactState';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
